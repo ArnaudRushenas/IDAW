f5d793ed8ae158cb9d44cef3374f8209ca89cb22
Arnaud Rushenas <arnaud.rushenas@gmail.com>
2021-04-13 11:13:46 +0200

    clear changes

---
+++ TP2/exo3/cv.php
@@ -0,0 +1,17 @@
+<?php
+require_once('template_header.php');
+?>
+
+<nav class="menu">
+    <ul>
+        <li><a href="index.html">Index</a></li>
+        <li><a href="cv.html">CV</a></li>
+        <li><a href="projets.html">Projets</a></li>
+    </ul>
+</nav>
+<h1>CV</h1>
+<p>Voici ci-dessous mon CV</p>
+<a href="file:///C:/3%20-%20ARNAUD/CV/Arnaud%20Rushenas-%20CV%20(1).pdf">Mon CV</a>
+</body>
+</html>
+
---
+++ TP2/exo3/index.php
@@ -0,0 +1,32 @@
+<?php
+require_once('template_header.php');
+?>
+
+
+
+
+<nav class="menu">
+    <ul>
+    <li><a href="index.html">Index</a></li>
+    <li><a href="cv.html">CV</a></li>
+    <li><a href="projets.html">Projets</a></li>
+    </ul>
+    </nav>
+    
+    <header>Page d'accueil du site</header>
+        <article>
+            <header>Qui suis-je ?</header>
+            <section>Arnaud Rushenas</section>
+            <section>Etudiant à l'IMT-Lille-Douai</section>
+            <footer>OoOooOOOO</footer>
+        </article>
+        <div>
+            <aside>Cliquez sur "CV" ci-dessus pour plus d'informations</aside>
+            <aside>Partie en bas à droite de la page</aside>
+        </div>
+        <footer>Fin de cette page</footer>
+    </body>
+
+    
+        
+            
